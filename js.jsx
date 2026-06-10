405fd6a2de90845513ea5c3812a62d58e1f16daa
Lênon Cazatti Belotti <lenoncazattibelotti@gmail.com>
2026-06-09 23:32:37 -0300

    Add files via upload

--- js.jsx
+++ js.jsx
@@ -1,6 +1,14 @@
-const visEl = document.getElementById("vis");
-const root = ReactDOM.createRoot(visEl);
 
+
+
+
+
+
+const visEl = document.getElementById("vis");
+
+
+const root = ReactDOM.createRoot(visEl);
+function add(){
 let vi = Number(localStorage.getItem("acessos")) || 40;
 vi++;
 localStorage.setItem("acessos", vi);
@@ -8,48 +16,22 @@ localStorage.setItem("acessos", vi);
 root.render(
     <h1>Teve {vi} acessos</h1>
 );
+}
 
-
+add();
+// msg de erro
 const roEl = document.getElementById("ro");
-const roove = ReactDOM.createRoot(roEl);
-
+const ro = ReactDOM.createRoot(roEl);
 window.fordoar = function () {
-    roove.render(
+    ro.render(
         <div id="rove">
             <div className="aviso">
-                ERROR 404 <br />
                 Tá fora do ar
                 <br />
-                <button className="ac" onClick={() => roove.render(<></>)}>
-                    ok
+                <button className="ac" onClick={() => ro.render(<></>)}>
+                    ok 
                 </button>
             </div>
         </div>
     );
 };
-
-
-const han = document.getElementById("hamburger");
-const mob = document.getElementById("mobileMenu");
-
-if (han && mob) {
-
-    han.addEventListener("click", () => {
-
-        han.classList.toggle("active");
-        mob.classList.toggle("active");
-
-    });
-
-    document.querySelectorAll("#mobileMenu a").forEach(link => {
-
-        link.addEventListener("click", () => {
-
-            mob.classList.remove("active");
-            han.classList.remove("active");
-
-        });
- 
-    });
-
-}
